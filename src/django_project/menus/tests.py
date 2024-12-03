4cf17583c34961001b9fe08d74876899affb7c5c
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-12-03 16:45:43 +0100

    tests uploaded. missing views test

--- src/django_project/menus/tests.py
+++ src/django_project/menus/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
-
+from django.test import TestCase
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 # Create your tests here.
+
+# Testing views
+# class MenuTestCase(TestCase):
+#     def test_get_restaurants(self):
+#         post = self.client.post("http://localhost:8000/api/restaurants/", 
+#                                json={"name": "TestRestaurant"})
+#         response = self.client.get("http://localhost:8000/api/restaurants/")
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn('TestRestaurant', response.json())
+#         self.assertEqual(len(response.json()), 1)
+#         print(f"Restaurants: {response.json()}")    
+
+
+# # Testing models
+class RestaurantTest(TestCase):
+    def test_create_restaurant(self):
+        prior_length = Restaurant.objects.count()
+        # Create an instance of MyModel
+        restaurant = Restaurant.objects.create(name="Test Restaurant")
+        
+        # Check if the instance is saved correctly
+        self.assertEqual(restaurant.name, "Test Restaurant")
+        self.assertEqual(Restaurant.objects.count(), prior_length + 1)
+
+# # Testing serializers
+class RestaurantSerializerTest(TestCase):
+      def test_restaurant_serializer(self):
+        data = {'name': 'Test Restaurant'}
+        serializer = RestaurantSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['name'], 'Test Restaurant')
+ 
--- src/django_project/menus/views.py
+++ src/django_project/menus/views.py
@@ -5,7 +5,6 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
-from django.shortcuts import render
 from .models import (
     Restaurant, 
     Menu, 
